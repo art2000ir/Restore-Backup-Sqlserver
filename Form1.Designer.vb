e6d9d0d38417b274ba3ed5621c317cdad5247a5f
Alireza <art2000ir@gmail.com>
2024-03-28 02:09:42 +0330

    upload code for git
    برنامه تقریبا کامل است و فقط کدهای اضافه باید حذف شود

---
+++ Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.op = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Cb1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.FD = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Dp1 = New System.Windows.Forms.DateTimePicker()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(103, 218)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(226, 20)
+        Me.TextBox1.TabIndex = 323
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button2.Location = New System.Drawing.Point(352, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 322
+        Me.Button2.Text = "بازیابی اطلاعات"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'op
+        '
+        Me.op.FileName = "op"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(200, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "اتصال"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(103, 189)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(226, 20)
+        Me.TextBox2.TabIndex = 323
+        '
+        'Cb1
+        '
+        Me.Cb1.FormattingEnabled = True
+        Me.Cb1.Location = New System.Drawing.Point(201, 129)
+        Me.Cb1.Name = "Cb1"
+        Me.Cb1.Size = New System.Drawing.Size(153, 21)
+        Me.Cb1.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button3.Location = New System.Drawing.Point(352, 188)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 23)
+        Me.Button3.TabIndex = 326
+        Me.Button3.Text = "نسخه پستیبان"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label2.Location = New System.Drawing.Point(357, 132)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 13)
+        Me.Label2.TabIndex = 331
+        Me.Label2.Text = ":  انتخاب دیتابیس"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label1.Location = New System.Drawing.Point(356, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 330
+        Me.Label1.Text = ":  نام کاربری"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(201, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox3.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(201, 61)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox4.TabIndex = 2
+        Me.TextBox4.Text = "sa"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Button5.Location = New System.Drawing.Point(369, 254)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 332
+        Me.Button5.Text = "خروج"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label3.Location = New System.Drawing.Point(356, 44)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 330
+        Me.Label3.Text = ":  نام سرور"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(201, 44)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(153, 20)
+        Me.TextBox5.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(178, Byte))
+        Me.Label4.Location = New System.Drawing.Point(357, 88)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 13)
+        Me.Label4.TabIndex = 331
+        Me.Label4.Text = ":  کلمه عبور"
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Algerian", 7.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(325, 191)
+        Me.Button6.Name = "Button6"
+        Me.Button6.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Button6.Size = New System.Drawing.Size(25, 18)
+        Me.Button6.TabIndex = 324
+        Me.Button6.Text = "***"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Dp1
+        '
+        Me.Dp1.CustomFormat = "mm-hh-dd-MM-yyyy"
+        Me.Dp1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.Dp1.Location = New System.Drawing.Point(201, 12)
+        Me.Dp1.Name = "Dp1"
+        Me.Dp1.Size = New System.Drawing.Size(153, 20)
+        Me.Dp1.TabIndex = 333
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Algerian", 7.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(325, 220)
+        Me.Button4.Name = "Button4"
+        Me.Button4.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Button4.Size = New System.Drawing.Size(25, 18)
+        Me.Button4.TabIndex = 334
+        Me.Button4.Text = "***"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.TextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox6.Location = New System.Drawing.Point(108, 170)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(205, 13)
+        Me.TextBox6.TabIndex = 335
+        Me.TextBox6.Text = "For Example:    D:\FolderName\Filename"
+        Me.TextBox6.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 299)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Dp1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Cb1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "پشتیبان"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents op As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Cb1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents FD As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Dp1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,156 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Configuration
+Public Class Form1
+    Dim CON1 As New SqlConnection
+    Dim da, da_s As New SqlDataAdapter
+    Dim ds, ds_s As New DataSet
+    Dim com1, com2, com3 As SqlCommand
+    Dim read As SqlCommand
+    Dim serv, us, pas, daba, comstrl As String
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        On Error GoTo b
+        If TextBox1.Text.Trim.Length < 4 Then
+            MsgBox(" ابتدا فایل مورد نظر را انتخاب کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        daba = Cb1.Text
+        comstrl = "Data Source=" & serv & ";Initial Catalog=" & daba & ";User ID=" & us & ";Password=" & pas & ""
+        CON1 = New SqlConnection(comstrl)
+        CON1.Open()
+        com1 = New SqlCommand("ALTER DATABASE " & daba & " SET SINGLE_USER WITH ROLLBACK IMMEDIATE; use master RESTORE DATABASE " & daba & " FROM DISK ='" & TextBox1.Text & "' with replace ; ALTER DATABASE  " & daba & " SET  MULTI_USER WITH NO_WAIT ", CON1)
+        com1.CommandTimeout = 0
+        com1.ExecuteNonQuery()
+        CON1.Close()
+        MsgBox(" اطلاعات بازیابی شد", MsgBoxStyle.Information, "پیام")
+        TextBox1.Text = ""
+        Exit Sub
+b:
+        If Err.Number = 5 Then
+            MsgBox(" وضعیت اتصال را بررسی کنید و سپس نام دیتابیس مورد نظر را انتخاب کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        MsgBox(Err.Description)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        On Error GoTo c
+        Dim AdrBa As String
+        FD.ShowDialog()
+        AdrBa = FD.SelectedPath
+        TextBox2.Text = AdrBa
+        Exit Sub
+c:
+        MsgBox(Err.Description)
+    End Sub
+    Sub restoreback()
+        Dim comstr As String = "Data Source=" & Cb1.Text & ";Initial Catalog=" & Cb1.Text & ";User ID=" & TextBox4.Text & ";Password=" & TextBox3.Text & ""
+        CON1 = New SqlConnection(comstr)
+        CON1.Open()
+        da.SelectCommand = New SqlCommand()
+        da.SelectCommand.Connection = CON1
+        da.SelectCommand.CommandType = CommandType.StoredProcedure
+        da.SelectCommand.CommandText = "Sp_Restorwithoff"
+        da.SelectCommand.Parameters.AddWithValue("@patchFile", TextBox1.Text)
+        ds.Clear()
+        da.Fill(ds, "Sp_Restorwithoff")
+        CON1.Close()
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        On Error GoTo d
+        If TextBox5.Text.Trim.Length < 0 Then
+            MsgBox(" نام سرور را وارد کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox4.Focus()
+            Exit Sub
+        End If
+
+        If TextBox4.Text.Trim.Length < 0 Then
+            MsgBox(" نام کاربری را وارد کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox4.Focus()
+            Exit Sub
+        End If
+        serv = TextBox5.Text
+        us = TextBox4.Text
+        pas = TextBox3.Text
+        Cb1.Text = ""
+        Dim comstr As String = "Data Source=" & TextBox5.Text & ";Initial Catalog=master ;User ID=" & TextBox4.Text & ";Password=" & TextBox3.Text & ""
+        Loaddb()
+        Exit Sub
+d:
+        If Err.Number = 5 Then
+            MsgBox(" نام کاربری/نام سرور/کلمه عبور صحیح نیست .اتصال برقرار نشد", MsgBoxStyle.Critical, "خطا")
+            TextBox4.Focus()
+            Exit Sub
+        End If
+        MsgBox(Err.Description)
+    End Sub
+    Sub Loaddb()
+        Dim sdr As SqlDataReader
+        Dim rs As String
+        Dim cont, i As Integer
+        Dim comstr As String = "Data Source=" & TextBox5.Text & ";Initial Catalog=" & Cb1.Text & ";User ID=" & TextBox4.Text & ";Password=" & TextBox3.Text & ""
+        CON1 = New SqlConnection(comstr)
+        CON1.Open()
+        ds_s.Clear()
+        Cb1.DataBindings.Clear()
+        da_s.SelectCommand = New SqlCommand("EXEC sp_databases", CON1)
+        da_s.Fill(ds_s, "ip")
+        Cb1.DataBindings.Add(New Binding("datasource", ds_s, "ip"))
+        Cb1.DisplayMember = "database_Name"
+        Cb1.FormattingEnabled = True
+        Cb1.ValueMember = "remarks"
+        CON1.Close()
+
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        On Error GoTo s
+        If TextBox2.Text.Trim.Length < 3 Then
+            MsgBox(" مسیر ذخیره فایل را مشخص کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox2.Focus()
+            Exit Sub
+        End If
+        daba = Cb1.Text
+        comstrl = "Data Source=" & serv & ";Initial Catalog=" & daba & ";User ID=" & us & ";Password=" & pas & ""
+        CON1 = New SqlConnection(comstrl)
+        CON1.Open()
+        com1 = New SqlCommand("BACKUP DATABASE " & daba & " TO DISK ='" & TextBox2.Text & "" & Dp1.Text & "'  WITH COMPRESSION ", CON1)
+        com3 = New SqlCommand("Exec sp_configure 'backup compression default',1 ; Reconfigure with Override ;", CON1)
+        com3.ExecuteNonQuery()
+        com1.ExecuteNonQuery()
+
+        CON1.Close()
+        MsgBox(" تسخه پشتیبان تهیه شد", MsgBoxStyle.Information, "پیام")
+        TextBox2.Text = ""
+        Exit Sub
+s:
+        If Err.Number = 5 Then
+            MsgBox(" وضعیت اتصال را بررسی کنید و سپس نام دیتابیس مورد نظر را انتخاب کنید", MsgBoxStyle.Critical, "خطا")
+            TextBox1.Focus()
+            Exit Sub
+        End If
+        MsgBox(Err.Description)
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim adrRe As String
+        op.ShowDialog()
+        adrRe = op.FileName
+        TextBox1.Text = adrRe
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox5.Focus()
+    End Sub
+    Private Sub TextBox2_MouseHover(sender As Object, e As EventArgs) Handles TextBox2.MouseHover
+        TextBox6.Visible = True
+    End Sub
+
+    Private Sub TextBox2_MouseLeave(sender As Object, e As EventArgs) Handles TextBox2.MouseLeave
+        TextBox6.Visible = False
+    End Sub
+End Class
